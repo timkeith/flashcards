5e5ffc413d855d58a8a177ab90b63c6d566a6364
Tim Keith <tkeith@nvidia.com>
2023-03-14 10:15:24 -0700

    Updates

--- precache-manifest.3239d06e8b786ae131309d10ea1400e1.js
+++ precache-manifest.3239d06e8b786ae131309d10ea1400e1.js
@@ -20,24 +20,24 @@ self.__precacheManifest = [
     "url": "/counter-app/static/js/runtime~main.d4fdbeca.js"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/counter-app/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/counter-app/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "912ec66d7572ff821749319396470bde",
-    "url": "/counter-app/static/media/fontawesome-webfont.912ec66d.svg"
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/counter-app/static/media/fontawesome-webfont.fee66e71.woff"
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/counter-app/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
     "revision": "569a6758b02d781afbccf5dc70c5c435",
