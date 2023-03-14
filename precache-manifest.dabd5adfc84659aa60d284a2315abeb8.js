3d6dc60135708b9be7cb688196138287ce0e1f11
Tim Keith <tkeith@nvidia.com>
2023-03-14 10:21:32 -0700

    Updates

---
+++ precache-manifest.dabd5adfc84659aa60d284a2315abeb8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d185e31175572bdcfce",
+    "url": "/flashcards/static/css/main.fa06d1d5.chunk.css"
+  },
+  {
+    "revision": "6d185e31175572bdcfce",
+    "url": "/flashcards/static/js/main.6d185e31.chunk.js"
+  },
+  {
+    "revision": "66755cf221f0b44e48c0",
+    "url": "/flashcards/static/css/1.baa8d89d.chunk.css"
+  },
+  {
+    "revision": "66755cf221f0b44e48c0",
+    "url": "/flashcards/static/js/1.66755cf2.chunk.js"
+  },
+  {
+    "revision": "bf0c7529f098f9581a91",
+    "url": "/flashcards/static/js/runtime~main.bf0c7529.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/flashcards/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/flashcards/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/flashcards/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/flashcards/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/flashcards/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "bb12ed8ce3efbe483ca0085f433b8876",
+    "url": "/flashcards/index.html"
+  }
+];
