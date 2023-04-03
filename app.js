427c20590c7f877570a00bcc6bece8da86ca07a8
Tim Keith <tim@tkeith.com>
2023-04-03 12:59:41 -0700

    try to get /flashcards working

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import logger from 'morgan';
 import { fileURLToPath } from 'url';
 import GetWords from './src/GetWords.js';
 
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const staticDir = path.join(dirname, 'build');
+
 const router = express.Router();
 router.get('/words', (req, res, next) => {
   try {
@@ -14,21 +17,37 @@ router.get('/words', (req, res, next) => {
     next(err);
   }
 });
+router.get(/\/.*/, (req, res) => {
+  const url = req.url.replace(/^\/flashcards\//, '/');
+  if (url === '/words') {
+    console.error('bad GET /words');
+    //try {
+    //  GetWords(words => res.json(words));
+    //} catch (err) {
+    //  next(err);
+    //}
+  } else {
+    const file = path.join(staticDir, url);
+    console.log('send', file);
+    res.sendFile(path.join(staticDir, url));
+  }
+});
 
 var app = express();
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
 app.set('views', path.join(dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(dirname, 'build')));
+app.use(express.static(staticDir));
 app.use('/', router);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => { console.error('404:', res); return next(createError(404)); });
+app.use((req, res, next) => {
+  console.error('404:', req.url);
+  return next(createError(404));
+});
 
 app.use((error, req, res, next) => {
   console.error('error:', error);
